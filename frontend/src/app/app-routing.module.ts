7f91fa18811c01cc69a1ed9a8a0111e361b8d31e
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-12-20 18:10:36 -0300

    Criando agora a navegação e utilizando a criação padrão de router do CLI Angular,rotas de produto e rota padrão, retirada o href de todas as tags a e inserido o routerLink, possivel agora realizar a troca de componentes pelo navbar lateral

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import './views/home/home.component';
+import { HomeComponent } from './views/home/home.component';
+import './views/products-crud/products-crud.component';
+import { ProductsCrudComponent } from './views/products-crud/products-crud.component';
+
+
+// Cada Objeto desse array traz uma rota da aplicação (Criada ao iniciar o Angular CLI) onde agora sera permitido navegar pelo crud
+const routes: Routes = [{
+  path:"",
+  component:HomeComponent
+},
+{
+  path:"produtos",
+  component:ProductsCrudComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
