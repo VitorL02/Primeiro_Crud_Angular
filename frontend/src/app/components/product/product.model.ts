df673d9837399afd5ec285a327f2978ef8103f83
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-12-21 20:35:10 -0300

    Criando o modelo do produto

---
+++ frontend/src/app/components/product/product.model.ts
@@ -0,0 +1,8 @@
+
+// interface e especifico do typescript com ele foi definido todos os elementos do produto e a "?" e utilizada pois o id não e obrigatorio podendo ser gerado automaticamente
+
+export interface Produto{
+    id?: number,
+    name:string,
+    price:number
+}
