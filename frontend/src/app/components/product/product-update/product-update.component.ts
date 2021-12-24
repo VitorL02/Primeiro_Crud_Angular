ed77d69c3389ea6ad2448e2e55b7528ca6cbc5ca
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-12-24 11:40:11 -0300

    Corrigindo os cabeçalhos das telas de edição de produtos e deletar produtos inserindo o headerService no construtor de productUpdate e productDelete

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HeaderService } from '../../template/header/header.service';
 import { Produto } from '../product.model';
 import { ProductService } from '../product.service';
 
@@ -12,7 +13,13 @@ export class ProductUpdateComponent implements OnInit {
 
   produto: Produto;
 
-  constructor(private productService: ProductService, private router:Router, private route: ActivatedRoute ) { }
+  constructor(private productService: ProductService, private router:Router, private route: ActivatedRoute,private headerService:HeaderService) {
+    headerService.headerData={
+      title:'Editar Produto',
+      icon:'autorenew',
+      routerUrl:'/produtos'
+    }
+   }
 
   //Para que o formulario venha preenchido foi necessario receber o id via rotas assim conseguindo receber os dados do produto
   ngOnInit(): void {
