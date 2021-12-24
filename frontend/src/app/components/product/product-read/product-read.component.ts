1d794b081983cdd904382d7efb172da418194b78
Vitor Lucas <vitorcrispim@yahoo.com.br>
2021-12-24 00:07:58 -0300

    Criando um modelo e um service que realiza a troca do cabeçalho da aplicação para uma forma dinamica,onde que por meio dos parametros passados pelo arquivo .ts e possivel trocar o icone a rota e seu titulo

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../template/header/header.service';
 import { Produto } from '../product.model';
 import { ProductService } from '../product.service';
 
@@ -12,7 +13,13 @@ export class ProductReadComponent implements OnInit {
   produtos: Produto[] = []; 
   displayedColumns = ['id','name','price','action'];
 
-  constructor(private produtoService:ProductService) { }
+  constructor(private produtoService:ProductService,private headerService:HeaderService) { 
+    headerService.headerData={
+      title:'Lista de Produtos',
+      icon:'storefront',
+      routerUrl:'/produtos'
+    }
+  }
 
   ngOnInit(): void {
     this.produtoService.readProduct().subscribe(produtos =>{ 
